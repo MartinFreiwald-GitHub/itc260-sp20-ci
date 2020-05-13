92fb0b384be591015a112887d6f0ec360eca37d2
MartinFreiwald-GitHub <MartinFreiwald@yahoo.com>
2020-05-12 20:54:47 -0700

    fixed api in custom_config
    Added api key and in Pics.php added api_key = flickrkey.

---
+++ 20200512_api_works_now_see_views/application/controllers/News.php
@@ -0,0 +1,80 @@
+<?php
+
+//application/controllers/News.php
+class News extends CI_Controller {
+
+        public function __construct()
+        {
+                parent::__construct();
+                $this->config->set_item('banner', 'News Section');
+                $this->load->model('news_model');
+                $this->load->helper('url_helper');
+        }
+
+        public function index()
+        {
+                $nav1 = $this->config->item('nav1');
+                var_dump($nav1);
+                $this->config->set_item('title', 'Seattle Sports News');
+                $data['title'] = 'news archive';
+                $this->load->view('news/index', $data);   
+        }
+
+        public function view($slug = NULL)
+        {
+                // google search -
+                // php replace dashes with space  ; // https://stackoverflow.com/questions/13546597/replace-dash-with-space-in-php
+                // google search -
+                // php title case ; https://www.php.net/manual/en/function.ucwords.php
+
+                //slug without dashes
+                $dashless_slug = str_replace(" ", " ", $slug);
+
+                // uppercase slug words
+                $dashless_slug = ucwords($dashless_slug);  
+
+                //use slug for title
+                $this->config->set_item($dashless_slug);
+
+                // add news flash
+                $this->config->set_item('title', 'News Flash - ' . $dashless_slug);
+
+                $data['news_item'] = $this->news_model->get_news($slug);
+        
+                if (empty($data['news_item']))
+                {
+                        show_404();
+                }
+                $data['title'] = $data['news_item']['title'];
+                $this->load->view('news/view', $data);
+        }
+
+        public function create(){
+                $this->load->helper('form');
+                $this->load->library('form_validation');
+            
+                $data['title'] = 'Create a news item';
+            
+                $this->form_validation->set_rules('title', 'Title', 'required');
+                $this->form_validation->set_rules('text', 'Text', 'required');
+            
+                if ($this->form_validation->run() === FALSE)
+                {
+                    $this->load->view('news/create', $data);
+                }
+                else
+                {
+                        //     $this->news_model->set_news();
+                        //     $this->load->view('news/success');
+
+                        $slug = $this->news_model->set_news();
+                        if($slug !== false){
+                                feedback('data entered successfully', 'info');
+                                redirect('news/view/' . $slug); 
+                        }else{
+                                feedback('data not entered', 'error');
+                                redirect('news/create');        
+                        }
+                }
+        }// end of create
+}
---
+++ 20200512_api_works_now_see_views/application/views/pages/about.php
@@ -0,0 +1,7 @@
+<?php //application/views/pages/about.php?>
+
+
+<h1>Welcome to my home page!</h1>
+<p>Any Slugs you find here will be well behaved!</p>
+<p>If you find them in the bathroom, please knock before entering!</p>
+
